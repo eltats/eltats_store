22338f8d3f98f33302a1d7c9d2804a20058e0f9e
ELTATS <artisank@student.21-school.ru>
2023-02-27 11:57:47 +0300

    log test

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"eltats_store/internal/app/apiserver"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
